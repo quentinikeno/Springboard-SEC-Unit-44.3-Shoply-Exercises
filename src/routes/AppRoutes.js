07bc1193a893300949c01352d5179b68c4d47fbb
Quentin Ikeno <ikenoq@gmail.com>
2022-08-07 16:55:22 -1000

    Add routes for product details.

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,10 +1,12 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Inventory from "../Inventory";
+import ProductDetail from "../ProductDetail";
 
 const AppRoutes = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Inventory />} />
+			<Route path="/products/:id" element={<ProductDetail />} />
 			<Route path="*" element={<Navigate replace to="/" />} />
 		</Routes>
 	);
