76d56db4dd914eed899e3c69f14d397736d7684e
Quentin Ikeno <ikenoq@gmail.com>
2022-08-07 15:51:14 -1000

    Wrap App with BrowserRouter.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import reportWebVitals from "./reportWebVitals";
 
 import { store } from "./reducer/store";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
 		</Provider>
 	</React.StrictMode>
 );
