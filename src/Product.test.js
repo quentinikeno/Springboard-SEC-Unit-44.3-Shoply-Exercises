ab72d3c3bb05accff961cd77e4b72b6c3a599087
Quentin Ikeno <ikenoq@gmail.com>
2022-08-07 15:34:18 -1000

    Add test to add and remove items from cart.

--- src/Product.test.js
+++ src/Product.test.js
@@ -1,6 +1,7 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import { store } from "./reducer/store";
 import { Provider } from "react-redux";
+import { clearCart } from "./reducer/productsSlice";
 import Product from "./Product";
 
 const tvId = "47314fa1-ae56-4eae-80be-af6691145951";
@@ -12,20 +13,61 @@ const tv = {
 	image_url:
 		"https://images.samsung.com/is/image/samsung/latin_en-hd-j4300ah-un32j4300ahxpa-001-front-indigo-blue",
 };
-
-it("renders without crashing", () => {
-	render(
+const addTV = () => {
+	const { queryByText, getByText } = render(
 		<Provider store={store}>
 			<Product id={tvId} product={tv} />
 		</Provider>
 	);
+	// Remove from cart button not in the document
+	expect(queryByText("Remove")).not.toBeInTheDocument();
+
+	// Test clicking on the add to cart button.
+	const addToCartBtn = getByText("Add");
+	fireEvent.click(addToCartBtn);
+	return { getByText, queryByText };
+};
+
+afterEach(() => {
+	clearCart();
 });
 
-it("renders matches snapshot", () => {
-	const { asFragment } = render(
+describe("Smoke and snapshot tests.", () => {
+	it("renders without crashing", () => {
+		render(
+			<Provider store={store}>
+				<Product id={tvId} product={tv} />
+			</Provider>
+		);
+	});
+
+	it("renders matches snapshot", () => {
+		const { asFragment } = render(
+			<Provider store={store}>
+				<Product id={tvId} product={tv} />
+			</Provider>
+		);
+		expect(asFragment()).toMatchSnapshot();
+	});
+});
+
+it("Adds the TV to cart when the add to cart button is clicked.", () => {
+	const { queryByText, getByText } = render(
 		<Provider store={store}>
 			<Product id={tvId} product={tv} />
 		</Provider>
 	);
-	expect(asFragment()).toMatchSnapshot();
+	// Remove from cart button not in the document
+	expect(queryByText("Remove")).not.toBeInTheDocument();
+
+	// Test clicking on the add to cart button.
+	const addToCartBtn = getByText("Add");
+	fireEvent.click(addToCartBtn);
+	expect(getByText("Remove")).toBeInTheDocument();
+	expect(getByText("1")).toBeInTheDocument();
+
+	const removeFromCartBtn = getByText("Remove");
+	fireEvent.click(removeFromCartBtn);
+	expect(queryByText("Remove")).not.toBeInTheDocument();
+	expect(queryByText("1")).not.toBeInTheDocument();
 });
