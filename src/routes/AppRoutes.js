743460d1bf01e25231e74599b38ac1ff8c2a838a
Quentin Ikeno <ikenoq@gmail.com>
2022-08-08 18:27:03 -1000

    Add React Router route for Cart.js

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,12 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Inventory from "../Inventory";
 import ProductDetail from "../ProductDetail";
+import Cart from "../Cart";
 
 const AppRoutes = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Inventory />} />
 			<Route path="/products/:id" element={<ProductDetail />} />
+			<Route path="/cart" element={<Cart />} />
 			<Route path="*" element={<Navigate replace to="/" />} />
 		</Routes>
 	);
