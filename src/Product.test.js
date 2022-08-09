4362971fb17eb0d8055e37020bbc5105a7e13fb9
Quentin Ikeno <ikenoq@gmail.com>
2022-08-08 19:23:59 -1000

    Update tests to use MemoryRouter.

--- src/Product.test.js
+++ src/Product.test.js
@@ -1,7 +1,7 @@
 import { fireEvent, render } from "@testing-library/react";
 import { store } from "./reducer/store";
 import { Provider } from "react-redux";
-import { clearCart } from "./reducer/productsSlice";
+import { MemoryRouter } from "react-router-dom";
 import Product from "./Product";
 
 const tvId = "47314fa1-ae56-4eae-80be-af6691145951";
@@ -13,30 +13,14 @@ const tv = {
 	image_url:
 		"https://images.samsung.com/is/image/samsung/latin_en-hd-j4300ah-un32j4300ahxpa-001-front-indigo-blue",
 };
-const addTV = () => {
-	const { queryByText, getByText } = render(
-		<Provider store={store}>
-			<Product id={tvId} product={tv} />
-		</Provider>
-	);
-	// Remove from cart button not in the document
-	expect(queryByText("Remove")).not.toBeInTheDocument();
-
-	// Test clicking on the add to cart button.
-	const addToCartBtn = getByText("Add");
-	fireEvent.click(addToCartBtn);
-	return { getByText, queryByText };
-};
-
-afterEach(() => {
-	clearCart();
-});
 
 describe("Smoke and snapshot tests.", () => {
 	it("renders without crashing", () => {
 		render(
 			<Provider store={store}>
-				<Product id={tvId} product={tv} />
+				<MemoryRouter>
+					<Product id={tvId} product={tv} />
+				</MemoryRouter>
 			</Provider>
 		);
 	});
@@ -44,7 +28,9 @@ describe("Smoke and snapshot tests.", () => {
 	it("renders matches snapshot", () => {
 		const { asFragment } = render(
 			<Provider store={store}>
-				<Product id={tvId} product={tv} />
+				<MemoryRouter>
+					<Product id={tvId} product={tv} />
+				</MemoryRouter>
 			</Provider>
 		);
 		expect(asFragment()).toMatchSnapshot();
@@ -54,7 +40,9 @@ describe("Smoke and snapshot tests.", () => {
 it("Adds the TV to cart when the add to cart button is clicked.", () => {
 	const { queryByText, getByText } = render(
 		<Provider store={store}>
-			<Product id={tvId} product={tv} />
+			<MemoryRouter>
+				<Product id={tvId} product={tv} />
+			</MemoryRouter>
 		</Provider>
 	);
 	// Remove from cart button not in the document
